47c07d39af0ceee890086c7e987743ca2d6bf38f
MilesofPurple <125920441+MilesofPurple@users.noreply.github.com>
2023-02-20 16:28:30 -0800

    Add files via upload

---
+++ Visual Basic/Arithmetic/Form1.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.txtNumber1 = New System.Windows.Forms.TextBox()
+        Me.txtNumber2 = New System.Windows.Forms.TextBox()
+        Me.lblNumber1 = New System.Windows.Forms.Label()
+        Me.lblNumber2 = New System.Windows.Forms.Label()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtNumber1
+        '
+        Me.txtNumber1.Location = New System.Drawing.Point(715, 530)
+        Me.txtNumber1.Name = "txtNumber1"
+        Me.txtNumber1.Size = New System.Drawing.Size(326, 31)
+        Me.txtNumber1.TabIndex = 0
+        '
+        'txtNumber2
+        '
+        Me.txtNumber2.Location = New System.Drawing.Point(715, 601)
+        Me.txtNumber2.Name = "txtNumber2"
+        Me.txtNumber2.Size = New System.Drawing.Size(326, 31)
+        Me.txtNumber2.TabIndex = 1
+        '
+        'lblNumber1
+        '
+        Me.lblNumber1.AutoSize = True
+        Me.lblNumber1.Location = New System.Drawing.Point(617, 536)
+        Me.lblNumber1.Name = "lblNumber1"
+        Me.lblNumber1.Size = New System.Drawing.Size(92, 25)
+        Me.lblNumber1.TabIndex = 2
+        Me.lblNumber1.Text = "Number 1"
+        '
+        'lblNumber2
+        '
+        Me.lblNumber2.AutoSize = True
+        Me.lblNumber2.Location = New System.Drawing.Point(617, 607)
+        Me.lblNumber2.Name = "lblNumber2"
+        Me.lblNumber2.Size = New System.Drawing.Size(92, 25)
+        Me.lblNumber2.TabIndex = 3
+        Me.lblNumber2.Text = "Number 2"
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(729, 698)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(269, 138)
+        Me.btnCalculate.TabIndex = 4
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1703, 978)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.lblNumber2)
+        Me.Controls.Add(Me.lblNumber1)
+        Me.Controls.Add(Me.txtNumber2)
+        Me.Controls.Add(Me.txtNumber1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtNumber1 As TextBox
+    Friend WithEvents txtNumber2 As TextBox
+    Friend WithEvents lblNumber1 As Label
+    Friend WithEvents lblNumber2 As Label
+    Friend WithEvents btnCalculate As Button
+End Class
---
+++ Visual Basic/Arithmetic/Form1.vb
@@ -0,0 +1,37 @@
+﻿Public Class Form1
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+
+        Dim dblNumber1 As Double
+        Dim dblNumber2 As Double
+
+        Dim dblResult As Double
+
+        dblNumber1 = txtNumber1.Text
+        dblNumber2 = txtNumber2.Text
+
+        dblResult = dblNumber1 + dblNumber2
+        MsgBox("The sum of " & dblNumber1 & " and " & dblNumber2 & " is: " & dblResult)
+
+        dblResult = dblNumber1 - dblNumber2
+        MsgBox("The difference between " & dblNumber1 & " and " & dblNumber2 & " is: " & dblResult)
+
+        dblResult = dblNumber1 * dblNumber2
+        MsgBox("The product of " & dblNumber1 & " and " & dblNumber2 & " is: " & dblResult)
+
+        dblResult = dblNumber1 / dblNumber2
+        MsgBox("The ratio of " & dblNumber2 & "s in " & dblNumber1 & " is: " & dblResult)
+
+        dblResult = dblNumber1 ^ dblNumber2
+        MsgBox(dblNumber1 & " raised to the power of " & dblNumber2 & " is: " & dblResult)
+
+        dblResult = dblNumber1 \ dblNumber2
+        MsgBox(dblNumber1 & " divided by " & dblNumber2 & " without remainder is: " & dblResult)
+
+        dblResult = dblNumber1 Mod dblNumber2
+        MsgBox("The remainder of " & dblNumber1 & " divided by " & dblNumber2 & " is: " & dblResult)
+
+
+    End Sub
+
+
+End Class
