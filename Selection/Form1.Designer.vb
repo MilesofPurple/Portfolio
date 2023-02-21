06ac2b69094ccc4794a611458b5f2a02d98961e9
MilesofPurple <125920441+MilesofPurple@users.noreply.github.com>
2023-02-20 21:01:14 -0800

    Add files via upload

---
+++ Selection/Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtCountry = New System.Windows.Forms.TextBox()
+        Me.btnGreet = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(350, 214)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(235, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "What country are you from?"
+        '
+        'txtCountry
+        '
+        Me.txtCountry.Location = New System.Drawing.Point(591, 211)
+        Me.txtCountry.Name = "txtCountry"
+        Me.txtCountry.Size = New System.Drawing.Size(267, 31)
+        Me.txtCountry.TabIndex = 1
+        '
+        'btnGreet
+        '
+        Me.btnGreet.Location = New System.Drawing.Point(591, 298)
+        Me.btnGreet.Name = "btnGreet"
+        Me.btnGreet.Size = New System.Drawing.Size(267, 163)
+        Me.btnGreet.TabIndex = 2
+        Me.btnGreet.Text = "Greeting"
+        Me.btnGreet.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1294, 804)
+        Me.Controls.Add(Me.btnGreet)
+        Me.Controls.Add(Me.txtCountry)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtCountry As TextBox
+    Friend WithEvents btnGreet As Button
+End Class
