43cae2366629db7f27007ab358656d57f8f64e38
MilesofPurple <125920441+MilesofPurple@users.noreply.github.com>
2023-02-21 21:43:42 -0800

    Add files via upload

---
+++ Visual Basic/DoLoopValidation/Form1.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form1
+    Private Sub btnGo_Click(sender As Object, e As EventArgs) Handles btnGo.Click
+
+        Dim stName As String
+
+        Do While True
+            stName = InputBox("Please enter your name")
+            If stName = "MILES" Then
+                Exit Do
+            End If
+        Loop
+
+        MsgBox("Hello " & stName)
+
+    End Sub
+
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+
+        Dim stAge As String
+        Dim iAge As Integer
+
+        Do While IsNumeric(stAge) = False
+            stAge = InputBox("Please enter your age in years")
+        Loop
+
+        iAge = CInt(stAge)
+
+        MsgBox("Hello, you are " & iAge & " years old.")
+    End Sub
+End Class
---
+++ Visual Basic/DoWhileLoops/Form1.vb
@@ -0,0 +1,39 @@
+﻿Public Class Form1
+
+    Private Sub btnGo_Click(sender As Object, e As EventArgs) Handles btnGo.Click
+        ''Do-loop examples of different wording with the same output
+        Dim iCount As Integer
+
+        iCount = 0
+
+        Do While iCount < 5
+            iCount = iCount + 1
+            MsgBox("Hello " & iCount)
+        Loop
+
+        iCount = 0
+
+        Do
+            iCount = iCount + 1
+            MsgBox("Hello " & iCount)
+        Loop While iCount < 5
+
+        iCount = 0
+
+        Do Until iCount = 5
+            iCount = iCount + 1
+            MsgBox("Hello " & iCount)
+        Loop
+
+        iCount = 0
+
+        Do
+            iCount = iCount + 1
+            MsgBox("Hello " & iCount)
+        Loop Until iCount = 5
+
+        MsgBox("All done!")
+
+    End Sub
+
+End Class
