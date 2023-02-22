40b0510682650fcf05e4ffb928d0dfd2462cd284
MilesofPurple <125920441+MilesofPurple@users.noreply.github.com>
2023-02-21 21:42:27 -0800

    Add files via upload

---
+++ Visual Basic/CountUp/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+    Private Sub btnCount_Click(sender As Object, e As EventArgs) Handles btnCount.Click
+        Dim decNumber As Decimal
+        Dim stOddOrEven As String
+        Dim iCount As Integer
+
+
+        decNumber = InputBox("What number do you want to count up to?")
+        stOddOrEven = InputBox("Do you want odd numbers or even numbers?")
+
+        If stOddOrEven.ToUpper = "ODD" Then
+            For iCount = 1 To decNumber Step 2
+                MsgBox(iCount)
+            Next
+        ElseIf stOddOrEven.ToUpper = "EVEN" Then
+            For iCount = 0 To decNumber Step 2
+                MsgBox(iCount)
+            Next
+        End If
+
+
+    End Sub
+End Class
---
+++ Visual Basic/ForNext/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub btnGo_Click(sender As Object, e As EventArgs) Handles btnGo.Click
+
+        Dim iCount As Integer
+        Dim stOut As String
+
+        For iCount = 50 To 0 Step -5
+            stOut = stOut & iCount & vbNewLine
+            MsgBox("Hello!" & iCount)
+            MsgBox("How are you?")
+            MsgBox("Well, I hope.")
+        Next
+    End Sub
+End Class
