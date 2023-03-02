8ed654a670e2585bc07ce1ec6f43018ddec2c846
MilesofPurple <125920441+MilesofPurple@users.noreply.github.com>
2023-03-02 08:01:35 -0800

    Update Form1.vb

--- Visual Basic/Selection/Form1.vb
+++ Visual Basic/Selection/Form1.vb
@@ -1,8 +1,11 @@
 ﻿Public Class Form1
     Private Sub btnGreet_Click(sender As Object, e As EventArgs) Handles btnGreet.Click
+        
+        'Declare variables
         Dim stCountry As String
         stCountry = txtCountry.Text
 
+        'Search user input for specific countries and display custom messages. 
         If stCountry.ToUpper = "AUSTRAILIA" Then
             MsgBox("G'day mate!")
             MsgBox("Good on ya!")
