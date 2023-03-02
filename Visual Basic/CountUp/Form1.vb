0572b798df6254a99d277fa6cfb55731bf3954de
MilesofPurple <125920441+MilesofPurple@users.noreply.github.com>
2023-03-01 19:36:02 -0800

    Update Form1.vb

--- Visual Basic/CountUp/Form1.vb
+++ Visual Basic/CountUp/Form1.vb
@@ -1,13 +1,19 @@
 ﻿Public Class Form1
     Private Sub btnCount_Click(sender As Object, e As EventArgs) Handles btnCount.Click
+        
+        'Declare variables
         Dim decNumber As Decimal
         Dim stOddOrEven As String
         Dim iCount As Integer
 
-
+        'Ask how high we want to count up to
         decNumber = InputBox("What number do you want to count up to?")
-        stOddOrEven = InputBox("Do you want odd numbers or even numbers?")
-
+        
+        'Ask if we want to display even or odd numbers
+        dOrEven = InputBox("Do you want odd numbers or even numbers?")
+        
+        
+        'Display odd or even numbers depending on user selection
         If stOddOrEven.ToUpper = "ODD" Then
             For iCount = 1 To decNumber Step 2
                 MsgBox(iCount)
