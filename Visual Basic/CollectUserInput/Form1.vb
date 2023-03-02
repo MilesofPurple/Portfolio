f0c160209dd1c8b9ad2246fd6220cb3c3856e597
MilesofPurple <125920441+MilesofPurple@users.noreply.github.com>
2023-03-01 19:23:13 -0800

    Update Form1.vb

--- Visual Basic/CollectUserInput/Form1.vb
+++ Visual Basic/CollectUserInput/Form1.vb
@@ -1,18 +1,20 @@
 ﻿Public Class Form1
     Private Sub btnGo_Click(sender As Object, e As EventArgs) Handles btnGo.Click
-
+        
+        'Declare variables
         Dim stFirstName As String
         Dim stLastName As String
         Dim stGender As String
         Dim stOccupation As String
-
+        
+        'Assign user inputs to variables
         stFirstName = txtFirstName.Text
         stLastName = txtLastName.Text
         stGender = txtGender.Text
 
         stOccupation = lstOccupation.SelectedItem
 
-
+        'Display user selections
         MsgBox("Hello " & stFirstName & " " & stLastName & "!  You are a " & stGender & " " & stOccupation & "!")
 
 
@@ -20,6 +22,7 @@
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Load additional jobs into program
         lstOccupation.Items.Add("Writer")
         lstOccupation.Items.Add("Actor")
         lstOccupation.Items.Add("Painter")
