1889bb174ac8ea96fcb567d4e79e4cd76fa76afc
MilesofPurple <125920441+MilesofPurple@users.noreply.github.com>
2023-03-01 19:42:38 -0800

    Update Form1.vb

--- Visual Basic/DoLoopValidation/Form1.vb
+++ Visual Basic/DoLoopValidation/Form1.vb
@@ -2,7 +2,8 @@
     Private Sub btnGo_Click(sender As Object, e As EventArgs) Handles btnGo.Click
 
         Dim stName As String
-
+        
+        'Keep asking for a name until MILES is entered
         Do While True
             stName = InputBox("Please enter your name")
             If stName = "MILES" Then
@@ -19,12 +20,15 @@
         Dim stAge As String
         Dim iAge As Integer
 
+        'Check if input is a number
         Do While IsNumeric(stAge) = False
             stAge = InputBox("Please enter your age in years")
         Loop
-
+        
+        'Convert age from string to integer and assign it to an integer variable
         iAge = CInt(stAge)
-
+        
+        'Display age
         MsgBox("Hello, you are " & iAge & " years old.")
     End Sub
 End Class
