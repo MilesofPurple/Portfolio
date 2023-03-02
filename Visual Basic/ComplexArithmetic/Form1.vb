8d73353c4a9e1ade1292bfea3b56699a4e9f457e
MilesofPurple <125920441+MilesofPurple@users.noreply.github.com>
2023-03-01 19:29:43 -0800

    Update Form1.vb

--- Visual Basic/ComplexArithmetic/Form1.vb
+++ Visual Basic/ComplexArithmetic/Form1.vb
@@ -1,24 +1,29 @@
 ﻿Public Class Form1
     Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        
+        'Declare variables
         Dim decPrice As Decimal
         Dim iQuantity As Integer
         Dim decTotalCost As Decimal
         Dim decDiscount As Decimal
         Dim decPostage As Decimal
 
-
+        'Assign values to variables
         decPrice = 5
         iQuantity = 10
         decDiscount = 2
         decPostage = 3
-
+        
+        'Calculate total cost to mail
         decTotalCost = (decPrice - decDiscount) * iQuantity + decPostage
-
+        
+        'Display total cost
         MsgBox(decTotalCost)
 
         'BO(DM)(AS) - Brackets Order Division Multiplication Addition Subtraction
         'PE(MD)(AS) - Parentheses Exponentiation Multiplication Division Addition Subtraction
-
+        
+        
         Dim iResult As Integer
         iResult = 10 - 5 + 2
         MsgBox(iResult)
