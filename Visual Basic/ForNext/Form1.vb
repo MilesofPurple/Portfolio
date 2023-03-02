9a622f1484ca919803a5f4bfebdbd624d13744c8
MilesofPurple <125920441+MilesofPurple@users.noreply.github.com>
2023-03-02 07:35:10 -0800

    Update Form1.vb

--- Visual Basic/ForNext/Form1.vb
+++ Visual Basic/ForNext/Form1.vb
@@ -1,9 +1,11 @@
 ﻿Public Class Form1
     Private Sub btnGo_Click(sender As Object, e As EventArgs) Handles btnGo.Click
-
+        
+        'Step tutorial
         Dim iCount As Integer
         Dim stOut As String
 
+        'Count down to 0 from 50 in increments of 5
         For iCount = 50 To 0 Step -5
             stOut = stOut & iCount & vbNewLine
             MsgBox("Hello!" & iCount)
